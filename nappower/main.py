b18e8bb0bfbacea5dfc616a6d57dc9a7e558ed47
baba5246 <baba_writer@yahoo.co.jp>
2020-10-24 09:49:33 +0900

    Add a template

--- nappower/main.py
+++ nappower/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="nappower/static"), name="static")
 
-@app.get("/hello")
-def hello():
-   return {"Hello": "World!"}
+
+templates = Jinja2Templates(directory="nappower/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
